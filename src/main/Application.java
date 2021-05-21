778954b47a3f553e1f0a00405518191264b9daab
Christianlopez4 <1107525635@u.icesi.edu.co>
2021-05-20 19:51:57 -0500

    Primera implementación del cifrado de archivos

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,9 +1,24 @@
 package main;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.util.List;
 import java.util.Scanner;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.swing.JFileChooser;
 
 public class Application {
@@ -13,7 +28,7 @@ public class Application {
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("Seleccione el archivo a cifrar o descifrar");
-        Scanner entrada = seleccionarArchivo();
+        File archivo = seleccionarArchivo();
 		
 		System.out.println("Seleccione la opción que desea realizar:");
         System.out.println("1. Cifrar archivo");
@@ -23,6 +38,21 @@ public class Application {
         System.out.println("Digite la contraseña del archivo");
         String contrasena = obtenerContrasena(scanner);
         
+	}
+	
+	public static File seleccionarArchivo() {
+		JFileChooser fileChooser = new JFileChooser();
+        fileChooser.showOpenDialog(fileChooser);
+        try {
+            String ruta = fileChooser.getSelectedFile().getAbsolutePath();                                        
+            File f = new File(ruta);
+            return f;
+        } catch (NullPointerException e) {
+            System.out.println("No se ha seleccionado ningún fichero");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } 
+		return null;
 	}
 	
 	public static int obtenerOpcion(Scanner scanner) {
@@ -43,31 +73,28 @@ public class Application {
 		return contrasena;
 	}
 	
-	public static Scanner seleccionarArchivo() {
-		Scanner entrada = null;
-		JFileChooser fileChooser = new JFileChooser();
-        fileChooser.showOpenDialog(fileChooser);
-        try {
-            String ruta = fileChooser.getSelectedFile().getAbsolutePath();                                        
-            File f = new File(ruta);
-            entrada = new Scanner(f);
-            return entrada;
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        } catch (NullPointerException e) {
-            System.out.println("No se ha seleccionado ningún fichero");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
-            if (entrada != null) {
-                entrada.close();
-            }
-        }
-		return entrada;
+	public void cifrarArchivo(File archivo, SecretKey llave) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, FileNotFoundException, IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance("AES");
+		cipher.init(Cipher.ENCRYPT_MODE, llave);
+		
+		FileInputStream inputStream = new FileInputStream(archivo);
+		byte[] cifrado = cipher.doFinal();
+	    Encoder encoder = Base64.getEncoder();
+		String byteToString = encoder.encodeToString(cifrado);
 	}
 	
-	public void cifrarArchivo() {
-		
+	public static SecretKey generarLlave(int n) {
+		 KeyGenerator keyGenerator;
+		try {
+			keyGenerator = KeyGenerator.getInstance("AES");
+			keyGenerator.init(n);
+		    SecretKey key = keyGenerator.generateKey();
+		    return key;
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	public void descifrarArchivo() {
